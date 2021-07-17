dfdb6df31426c09619a4ae95e0f5b3f851d3a803
inezabonte <inezabonte@gmail.com>
2021-07-18 01:38:37 +0200

    First Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Gatsby Newsletter",
-  },
-  plugins: [],
+	siteMetadata: {
+		siteUrl: "https://www.yourdomain.tld",
+		title: "Gatsby Newsletter",
+	},
+	plugins: ["gatsby-plugin-postcss"],
 };
